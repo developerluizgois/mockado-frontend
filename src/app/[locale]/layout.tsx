6f020b3657bba28b17a5419c4d35c2a9e5336a64
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-04-21 00:58:24 -0300

    feat: add shadcn/ui and navigation-menu component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "../globals.css";
+import Header from "@/components/ui/Header";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const open_sans = Open_Sans({
   subsets: ["latin"],
@@ -22,7 +24,17 @@ export default function LocaleLayout({
 }) {
   return (
     <html lang={locale}>
-      <body className={`${open_sans.variable} `}>{children}</body>
+      <body className={`${open_sans.className}`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
